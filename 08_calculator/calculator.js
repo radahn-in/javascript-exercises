177dc065ad0b01c514525935285c29e24d4fa847
radahn-in <raghu_nath1993@yahoo.com>
2023-08-18 22:09:05 +0530

    calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(myArray) {
+	let sum = 0;
+  for (let i = 0; i< myArray.length; i++){
+    sum += myArray[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  var sum=1;
+  for (var i=0; i<array.length; i++) {
+    sum *= array[i];
+  } 
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if(a === 0) return 1;
+  let fact = 1;
+  for(let i = a; i>0; i--){
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
