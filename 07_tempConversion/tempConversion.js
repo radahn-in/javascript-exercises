6338139a32c34742062334feabd97f08ec273d88
radahn-in <raghu_nath1993@yahoo.com>
2023-08-18 20:20:49 +0530

    tempConversion done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  const C = ((F - 32) * 5/9);
+  const rounded = Math.round(C * 10) / 10;
+  return rounded;
+   
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  const F = ((C * 9/5) + 32);
+  const rounded = Math.round(F * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
