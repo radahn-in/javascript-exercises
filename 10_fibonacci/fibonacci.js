46d9340bffe5a4c888967dc2f06390411a52227f
radahn-in <raghu_nath1993@yahoo.com>
2023-08-19 23:16:28 +0530

    fibo done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if(n<0) return "OOPS";
+    if(n === 0) return 0;
 
+    let a = 1;
+    let b = 0;
+    
+    for(let i = 2; i<=n; i++){
+        let c = a + b;
+        b = a;
+        a = c;
+    }
+    return a;
 };
 
 // Do not edit below this line
