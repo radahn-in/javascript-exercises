75774e013ac0a06ddf0dc715a24df0a01c953bd1
radahn-in <raghu_nath1993@yahoo.com>
2023-08-19 10:07:45 +0530

    palindrome done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const re = /[^A-Za-z0-9]/g;
+    const lowRegStr = str.toLowerCase().replace(re, '');
+    const reverseStr = lowRegStr.split('').reverse().join('');
+    return reverseStr === lowRegStr;
 };
 
 // Do not edit below this line
